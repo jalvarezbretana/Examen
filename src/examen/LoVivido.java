7870b2401e2e4abed30083a105e9bdce698df966
Jose Luis Álvarez Bretaña <jalvarezbretana@nautilus11.danielcastelao.org>
2019-11-29 13:29:39 +0100

    Tercer Commit LoVivido

---
+++ src/examen/LoVivido.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen;
+
+/**
+ *
+ * @author jalvarezbretana
+ */
+import javax.swing.JOptionPane;
+
+public class LoVivido {
+
+    public static void main(String[] args) {
+        String n;
+        String e;
+
+        int m;
+        int d;
+        int h;
+        int a;
+
+        n = JOptionPane.showInputDialog("Escriba su nombre: ");
+        e = JOptionPane.showInputDialog("Escriba su edad: ");
+
+        a = Integer.parseInt(e);
+
+        m = (a * 12);
+        d = (a * 365);
+        h = (d * 24);
+
+        JOptionPane.showMessageDialog(null, "Meses: " + m, "Numero de meses vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Días: " + d, "Numero de días vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Horas: " + h, "Numero de horas vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
+
+    }
+
+}
